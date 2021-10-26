f07708ae9b9ffbfc1f506f860d43f9801998276c
jsk1107 <seungkyu.korea@gmail.com>
2021-10-27 08:22:51 +0900

    inverval param 핸들링

--- train.py
+++ train.py
@@ -40,21 +40,60 @@ if torch.cuda.is_available():
 else:
     device = 'cpu'
 
-kwarg = {'w_bit': 32, 'a_bit': 32}
 
+def interval_param_list(model):
+    for name, param in model.named_parameters():
+        target = name.split('.')[-1].split('_')[-1]
+        if target == 'delta':
+            yield param
+
+
+def weight_param_list(model):
+    for name, param in model.named_parameters():
+        target = name.split('.')[-1].split('_')[-1]
+        if target != 'delta':
+            yield param
+
+
+kwarg = {'w_bit': 4, 'a_bit': 4}
 model = resnet20(**kwarg)
 criterion = torch.nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-4)
+optimizer = optim.SGD([{'params': interval_param_list(model), 'lr': 4e-4},
+                       {'params': weight_param_list(model), 'lr': 4e-2}], momentum=0.9, weight_decay=1e-4)
 scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100, 150, 180], gamma=0.1)
 model.to(device)
-#
-# model_path = './model_6.pt'
-# if model_path:
-#     print(f'load: {kwarg} | {model_path}')
-#
-#     checkpoint = torch.load(model_path)
-#     model.load_state_dict(checkpoint['state_dict'], strict=True)
-#     optimizer.load_state_dict(checkpoint['optim'])
+
+model_path = 'fp32_model_64epoch.pt'
+if model_path:
+    print(f'load: {kwarg} | {model_path}')
+
+    checkpoint = torch.load(model_path)
+    model.load_state_dict(checkpoint['state_dict'], strict=True)
+
+
+from qil import Transformer
+from torch.nn import Conv2d
+import matplotlib.pyplot as plt
+for name, module in model.named_modules():
+
+    # if isinstance(module, Conv2d):
+    #     for name_1, param in module.named_parameters():
+    #         if name_1 == 'weight':
+    #             tmp = param.view(-1).detach().cpu().numpy()
+    #             plt.hist(tmp)
+    #             plt.xlabel(f'{name} {name_1}')
+    #             plt.ylabel('count')
+    #             plt.show()
+
+    if isinstance(module, Transformer):
+        layer_name = module.name
+        if layer_name == 'weight':
+
+            module.c_delta.data = torch.nn.Parameter(torch.tensor([0.2], device='cuda'))
+            module.d_delta.data = torch.nn.Parameter(torch.tensor([0.15], device='cuda'))
+        if layer_name == 'activation':
+            module.c_delta.data = torch.nn.Parameter(torch.tensor([0.5], device='cuda'))
+            module.d_delta.data = torch.nn.Parameter(torch.tensor([0.45], device='cuda'))
 
 for EPOCH in range(EPOCHS):
 
@@ -107,7 +146,16 @@ for EPOCH in range(EPOCHS):
     acc = 100 * correct / total
     print(f'Acc: {acc:.4f}%')
 
-    # if EPOCH % 10 == 0:
-    check_point = {'state_dict': model.state_dict(),
-                   'optim': optimizer.state_dict()}
-    torch.save(check_point, f'./model_{EPOCH+1}.pt')
+    for name, module in model.named_modules():
+        if isinstance(module, Transformer):
+            layer_name = module.name
+            if layer_name == 'weight':
+                print(f'{name}{layer_name} || c_w: {round(module.c_delta.data.item(), 5)}, d_w: {round(module.d_delta.data.item(), 5)}')
+            if layer_name == 'activation':
+                print(f'{name}{layer_name} || c_x: {round(module.c_delta.data.item(), 5)}, d_x: {round(module.d_delta.data.item(), 5)}')
+
+    if EPOCH % 10 == 0:
+        check_point = {'state_dict': model.state_dict(),
+                       'optim': optimizer.state_dict(),
+                       'EPOCH': EPOCH}
+        torch.save(check_point, f'./model_{EPOCH+1}.pt')
