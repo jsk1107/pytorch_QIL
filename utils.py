813b21a93b69439518c959e3ba1d6710702f085a
jsk1107 <seungkyu.korea@gmail.com>
2021-10-28 11:44:19 +0900

    register_hook 제거 모듈 추가

--- utils.py
+++ utils.py
@@ -1,39 +1,36 @@
 import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
+import numpy as np
 
 class Hook:
 
-    def __init__(self, module, module_name, backward=False):
+    def __init__(self, module, module_name=None, backward=False):
+        self.module = module
         self.module_name = module_name
         if backward:
-            self.hook = module.register_backward_hook(self.hook_fn)
+            self.handle = module.register_backward_hook(self.hook_fn)
         else:
-            self.hook = module.register_forward_hook(self.hook_fn)
+            self.handle = module.register_forward_hook(self.hook_fn)
 
     def hook_fn(self, module, input, output):
 
-        if module.name == 'activation':
-            value = input[0].view(-1).detach().cpu().numpy()
-            plt.hist(value, bins=25)
-            plt.xlabel(f'{self.module_name} input')
-            plt.ylabel('count')
-            plt.show()
-            #
-            # value = output[0].view(-1).detach().cpu().numpy()
-            # plt.hist(value)
-            # plt.xlabel(f'{self.module_name} output')
-            # plt.ylabel('count')
-            # plt.show()
-
-    def remove(self):
-        self.hook.remove()
-
-    # if isinstance(module, Conv2d):
-    #     for name_1, param in module.named_parameters():
-    #         if name_1 == 'weight':
-    #             tmp = param.view(-1).detach().cpu().numpy()
-    #             plt.hist(tmp)
-    #             plt.xlabel(f'{name} {name_1}')
-    #             plt.ylabel('count')
-    #             plt.show()
+        print('------------', self.module_name, '.', self.module.name, '------------')
+        value = input[0].view(-1).detach().cpu().numpy()
+        value = value[value > 0]
+        percentile_90 = np.percentile(value, 90)
+        hist = np.histogram(value, bins=50)[1][1:]
+        hist = hist[hist < percentile_90]
+        prun_point, clip_point = np.min(hist), np.max(hist)
+        c_delta = (prun_point + clip_point) / 2
+        d_delta = clip_point - c_delta
+        module.c_delta.data = torch.nn.Parameter(torch.tensor([c_delta], device=0, dtype=torch.float))
+        module.d_delta.data = torch.nn.Parameter(torch.tensor([d_delta], device=0, dtype=torch.float))
+        #
+        # plt.hist(value, bins=30)
+        # plt.xlabel(f'{self.module_name} input')
+        # plt.ylabel('count')
+        # plt.show()
+#
+    # def remove(self):
+    #     self.handle.remove()
