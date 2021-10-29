850ea3e1cea9bf7cc2a7145d2fce26a0547c9248
jsk1107 <seungkyu.korea@gmail.com>
2021-10-29 18:18:38 +0900

    hook 분기 추가

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+# utils.py
 import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
@@ -8,6 +9,9 @@ class Hook:
     def __init__(self, module, module_name=None, backward=False):
         self.module = module
         self.module_name = module_name
+        self.weight = None
+        self.activation = None
+
         if backward:
             self.handle = module.register_backward_hook(self.hook_fn)
         else:
@@ -18,19 +22,38 @@ class Hook:
         print('------------', self.module_name, '.', self.module.name, '------------')
         value = input[0].view(-1).detach().cpu().numpy()
         value = value[value > 0]
-        percentile_90 = np.percentile(value, 90)
-        hist = np.histogram(value, bins=50)[1][1:]
-        hist = hist[hist < percentile_90]
+        out = output[0].view(-1).detach().cpu().numpy()
+        if self.module.name == 'weight':
+            self.weight = out
+            percentile_90 = np.percentile(value, 90)
+            hist = np.histogram(value, bins=50)[1][1:]
+            hist = hist[hist < percentile_90]
+        elif self.module.name == 'activation':
+            self.activation = out
+            percentile_90 = np.percentile(value, 90)
+            hist = np.histogram(value, bins=50)[1][1:]
+            hist = hist[hist < percentile_90]
+
+
         prun_point, clip_point = np.min(hist), np.max(hist)
         c_delta = (prun_point + clip_point) / 2
         d_delta = clip_point - c_delta
         module.c_delta.data = torch.nn.Parameter(torch.tensor([c_delta], device=0, dtype=torch.float))
         module.d_delta.data = torch.nn.Parameter(torch.tensor([d_delta], device=0, dtype=torch.float))
-        #
+
         # plt.hist(value, bins=30)
         # plt.xlabel(f'{self.module_name} input')
         # plt.ylabel('count')
         # plt.show()
-#
-    # def remove(self):
-    #     self.handle.remove()
+
+    def graph_prun_ratio(self):
+        # x_label = ''.join(self.module_name.split('.')[:3])
+
+        prun_ratio = []
+        for idx, value in enumerate(self.input):
+            prun_ratio.append((len(value) - np.count_nonzero(value)) / len(value))
+
+        plt.bar(self.input, prun_ratio)
+        plt.xlabel(f'{idx}')
+        plt.ylabel('Pruning Ratio')
+        plt.show()
