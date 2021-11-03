103a6160883938251ecfc36764885d69763f9e48
jsk1107 <seungkyu.korea@gmail.com>
2021-11-03 23:04:33 +0900

    resnet18 학습모듈 추가

--- train.py
+++ train.py
@@ -1,8 +1,9 @@
 import torch
 from torch.utils.data import DataLoader
-from torchvision.datasets import CIFAR10
+from torchvision.datasets import CIFAR10, ImageNet, ImageFolder
 from torchvision.transforms import transforms as T
-from resnet import resnet20
+from resnet import resnet20, resnet18
+# from torchvision.models.resnet import resnet18
 import torch.optim as optim
 from tqdm import tqdm
 from utils import Hook
@@ -17,7 +18,7 @@ logger = get_logger('./log', log_config='./logging.json')
 
 
 """ Data Setting """
-def custom_transfroms(train=True):
+def custom_transfroms_cifar10(train=True):
 
     if train:
         return T.Compose([ T.RandomCrop(32, padding=4),
@@ -32,15 +33,34 @@ def custom_transfroms(train=True):
                                        std=[0.229, 0.224, 0.225])
                            ])
 
-train_set = CIFAR10('/data', train=True, transform=custom_transfroms(True), download=True)
-val_set = CIFAR10('/data', train=False, transform=custom_transfroms(False))
+def custom_transfroms_imagenet(train=True):
 
-train_loader = DataLoader(train_set, batch_size=128, shuffle=True, pin_memory=True)
-val_loader = DataLoader(val_set, batch_size=128, shuffle=False, pin_memory=True)
+    if train:
+        return T.Compose([ T.Resize(size=(256, 256)),
+                           T.RandomCrop(224, padding=4),
+                           T.RandomHorizontalFlip(),
+                           T.ToTensor(),
+                           T.Normalize(mean=[0.485, 0.456, 0.406],
+                                       std=[0.229, 0.224, 0.225])
+                           ])
+    else:
+        return T.Compose([ T.ToTensor(),
+                           T.Normalize(mean=[0.485, 0.456, 0.406],
+                                       std=[0.229, 0.224, 0.225])
+                           ])
+
+# train_set = CIFAR10('../data', train=True, transform=custom_transfroms_cifar10(True), download=True)
+# val_set = CIFAR10('../data', train=False, transform=custom_transfroms_cifar10(False))
+
+train_set = ImageFolder('../data/imagenet/', transform=custom_transfroms_imagenet(True))
+val_set = ImageFolder('../data/imagenet/', transform=custom_transfroms_imagenet(False))
+
+train_loader = DataLoader(train_set, batch_size=256, shuffle=True, pin_memory=True)
+val_loader = DataLoader(val_set, batch_size=256, shuffle=False, pin_memory=True)
 
 """ CONFIG """
 
-EPOCHS = 100
+EPOCHS = 120
 
 if torch.cuda.is_available():
     device = 'cuda'
@@ -112,45 +132,47 @@ def check_interval_param(model):
 
 def main(**kwargs):
     pretrain_model_path = 'final_fp32_model.pt'
-    model = resnet20(pretrain_model_path, **kwargs)
+    # model = resnet20(pretrain_model_path, **kwargs)
+    model = resnet18(pretrain_model_path, **kwargs)
+    # model = resnet18(False)
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = optim.SGD([{'params': interval_param_list(model), 'lr': 1e-4},
                            {'params': weight_param_list(model), 'lr': 4e-2}], momentum=0.9, weight_decay=1e-4)
-    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[50, 70, 90], gamma=0.1)
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30, 60, 85, 95, 105], gamma=0.1)
     model.to(device)
 
-    # Hook 등록
-    handle_hooks = []
-    for module_name, module in model.named_modules():
-        if isinstance(module, Transformer):
-            # if module.name == 'activation':
-            h = Hook(module, module_name)
-            handle_hooks.append(h)
-
-    # 1024장 이미지를 통해 c_x, d_x 초기값 정하기
-    model.train()
-    tmp_dataloader = DataLoader(train_set, batch_size=1024, shuffle=True, pin_memory=True)
-    for data in tmp_dataloader:
-        optimizer.zero_grad()
-
-        imgs, targets = data
-        imgs, targets = imgs.to(device), targets.to(device)
-
-        output = model(imgs)
-        iter_loss = criterion(output, targets)
-        iter_loss.backward()
-        print('c_delta, d_delta 초기값 셋팅 완료')
-        break
-
-    # Interval Param Check
-    check_interval_param(model)
-
-    # Prun_ratio 그래프 그리기
-    graph_prun_ratio(handle_hooks, plot=False)
-
-    # Hook 제거
-    for handle_hook in handle_hooks:
-        handle_hook.handle.remove()
+    # # Hook 등록
+    # handle_hooks = []
+    # for module_name, module in model.named_modules():
+    #     if isinstance(module, Transformer):
+    #         # if module.name == 'activation':
+    #         h = Hook(module, module_name)
+    #         handle_hooks.append(h)
+    #
+    # # 1024장 이미지를 통해 c_x, d_x 초기값 정하기
+    # model.train()
+    # tmp_dataloader = DataLoader(train_set, batch_size=128, shuffle=True, pin_memory=True)
+    # for data in tmp_dataloader:
+    #     optimizer.zero_grad()
+    #
+    #     imgs, targets = data
+    #     imgs, targets = imgs.to(device), targets.to(device)
+    #
+    #     output = model(imgs)
+    #     iter_loss = criterion(output, targets)
+    #     iter_loss.backward()
+    #     print('c_delta, d_delta 초기값 셋팅 완료')
+    #     break
+    #
+    # # Interval Param Check
+    # check_interval_param(model)
+    #
+    # # Prun_ratio 그래프 그리기
+    # graph_prun_ratio(handle_hooks, plot=False)
+    #
+    # # Hook 제거
+    # for handle_hook in handle_hooks:
+    #     handle_hook.handle.remove()
 
     best_acc = .0
     for EPOCH in range(EPOCHS):
@@ -203,7 +225,6 @@ def main(**kwargs):
                 correct += (pred_idx == targets).sum()
 
         acc = 100 * correct / total
-        print(f'Acc: {acc:.4f}%')
 
         # Interval Param Check
         check_interval_param(model)
@@ -214,14 +235,18 @@ def main(**kwargs):
                            'optim': optimizer.state_dict(),
                            'EPOCH': EPOCH,
                            'Acc': best_acc.round()}
-            torch.save(check_point, f'./model_{EPOCH+1}.pt')
+            torch.save(check_point, f'./model_{kwargs.get("w_bit")}_{kwargs.get("a_bit")}_model.pt')
+
+        logger.info(f'EPOCH: {EPOCH + 1} | '
+                    f'Loss: {val_loss / (i + 1):.4f} | '
+                    f'Accuracy: {acc:.4f}%')
+    logger.info(f'Best_Acc: {best_acc:.4f}%')
 
 
 if __name__ == '__main__':
 
-
-    bits = [[32, 32], [2,2], [3,3], [4,4], [5,5]]
-
+    # bits = [[32, 32], [5, 5], [4, 4], [3, 3], [2, 2]]
+    bits = [[32, 32]]
     for i in bits:
         kwargs = {'w_bit': i[0], 'a_bit': i[1]}
         main(**kwargs)
