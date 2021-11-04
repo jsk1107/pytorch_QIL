80a5d5ac5d66fcb02f635c66e53109cab6ef1dce
jsk1107 <seungkyu.korea@gmail.com>
2021-11-04 23:41:05 +0900

    Hook 코드 정리

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 import numpy as np
+from qil import Transformer
+
 
 class Hook:
 
@@ -57,3 +59,67 @@ class Hook:
         plt.xlabel(f'{idx}')
         plt.ylabel('Pruning Ratio')
         plt.show()
+
+
+def interval_param_list(model):
+    for name, param in model.named_parameters():
+        target = name.split('.')[-1].split('_')[-1]
+        if target == 'delta':
+            yield param
+
+
+def weight_param_list(model):
+    for name, param in model.named_parameters():
+        target = name.split('.')[-1].split('_')[-1]
+        if target != 'delta':
+            yield param
+
+
+
+def graph_prun_ratio(handle_hooks, plot=False):
+    if not plot:
+        return
+
+    # prun ratio 그래프 그리기
+    activation_prun_ratio = []
+    weight_prun_ratio = []
+    for handle_hook in handle_hooks:
+        name = handle_hook.module.name
+        if name == 'weight':
+            inputs = handle_hook.weight
+            ratio = (len(inputs) - np.count_nonzero(inputs)) / len(inputs)
+            weight_prun_ratio.append(ratio)
+        elif name == 'activation':
+            inputs = handle_hook.activation
+            ratio = (len(inputs) - np.count_nonzero(inputs)) / len(inputs)
+            activation_prun_ratio.append(ratio)
+
+    xlim = np.arange(len(activation_prun_ratio))+1
+    plt.bar(xlim, activation_prun_ratio)
+    plt.xticks(xlim, xlim)
+    plt.ylabel('Activation Pruning Ratio')
+    plt.show()
+
+    xlim = np.arange(len(weight_prun_ratio))+1
+    plt.bar(xlim, weight_prun_ratio)
+    plt.xticks(xlim, xlim)
+    plt.ylabel('Weight Pruning Ratio')
+    plt.show()
+
+
+def check_interval_param(model):
+
+    for name, module in model.named_modules():
+        if isinstance(module, Transformer):
+            layer_name = module.name
+            if layer_name == 'weight':
+                print(
+                    f'{name}{layer_name} ||'
+                    f' c_w: {round(module.c_delta.data.item(), 5)},'
+                    f' d_w: {round(module.d_delta.data.item(), 5)}')
+            if layer_name == 'activation':
+                print(
+                    f'{name}{layer_name} ||'
+                    f' c_x: {round(module.c_delta.data.item(), 5)},'
+                    f' d_x: {round(module.d_delta.data.item(), 5)}')
+
