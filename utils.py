ca8ee93ebb6829a086994510d7e6f5c5afa4869d
jsk1107 <seungkyu.korea@gmail.com>
2022-03-27 19:36:26 +0900

    adjust hook & graph

--- utils.py
+++ utils.py
@@ -28,18 +28,18 @@ class Hook:
             self.weight = out
             percentile_90 = np.percentile(value, 90)
             value = value[value < percentile_90]
-            hist = np.histogram(value, bins=50)[1][1:]
+            hist = np.histogram(value, bins=10)[1][1:]
         elif self.module.name == 'activation':
             self.activation = out
             percentile_90 = np.percentile(value, 90)
             value = value[value < percentile_90]
-            hist = np.histogram(value, bins=50)[1][1:]
+            hist = np.histogram(value, bins=10)[1][1:]
 
         prun_point, clip_point = np.min(hist), np.max(hist)
         c_delta = (prun_point + clip_point) / 2
         d_delta = clip_point - c_delta
-        module.c_delta.data = torch.nn.Parameter(torch.tensor([c_delta], device=0, dtype=torch.float))
-        module.d_delta.data = torch.nn.Parameter(torch.tensor([d_delta], device=0, dtype=torch.float))
+        module.c_delta.data = torch.nn.Parameter(torch.tensor([c_delta], dtype=torch.float))
+        module.d_delta.data = torch.nn.Parameter(torch.tensor([d_delta], dtype=torch.float))
 
     def graph_prun_ratio(self):
         prun_ratio = []
@@ -67,50 +67,54 @@ def weight_param_list(model):
 
 
 
-def graph_prun_ratio(handle_hooks, plot=False):
-    if not plot:
-        return
+
+def prun_ratio(handle_hooks, plot=False):
 
     # prun ratio 그래프 그리기
-    activation_prun_ratio = []
-    weight_prun_ratio = []
+    weight_prun_ratio = {}
+    activation_prun_ratio = {}
     for handle_hook in handle_hooks:
         name = handle_hook.module.name
         if name == 'weight':
             inputs = handle_hook.weight
-            ratio = (len(inputs) - np.count_nonzero(inputs)) / len(inputs)
-            weight_prun_ratio.append(ratio)
+            ratio = np.count_nonzero(inputs == 0.) / len(inputs)
+
+            weight_prun_ratio[handle_hook.module_name] = ratio
         elif name == 'activation':
             inputs = handle_hook.activation
-            ratio = (len(inputs) - np.count_nonzero(inputs)) / len(inputs)
-            activation_prun_ratio.append(ratio)
+            ratio = np.count_nonzero(inputs == 0.) / len(inputs)
+            activation_prun_ratio[handle_hook.module_name] = ratio
 
-    xlim = np.arange(len(activation_prun_ratio))+1
-    plt.bar(xlim, activation_prun_ratio)
-    plt.xticks(xlim, xlim)
-    plt.ylabel('Activation Pruning Ratio')
-    plt.show()
+    if plot:
+        xlim = np.arange(len(activation_prun_ratio))+1
+        plt.bar(activation_prun_ratio.keys(), activation_prun_ratio.values())
+        plt.xticks(xlim, xlim)
+        plt.ylabel('Activation Pruning Ratio')
+        plt.show()
 
-    xlim = np.arange(len(weight_prun_ratio))+1
-    plt.bar(xlim, weight_prun_ratio)
-    plt.xticks(xlim, xlim)
-    plt.ylabel('Weight Pruning Ratio')
-    plt.show()
+        xlim = np.arange(len(weight_prun_ratio))+1
+        plt.bar(weight_prun_ratio.keys(), weight_prun_ratio.values())
+        # plt.bar(xlim, weight_prun_ratio)
+        plt.xticks(xlim, xlim)
+        plt.ylabel('Weight Pruning Ratio')
+        plt.show()
+
+    return weight_prun_ratio, activation_prun_ratio
 
 
-def check_interval_param(model):
+def check_interval_param(logger, model):
 
     for name, module in model.named_modules():
         if isinstance(module, Transformer):
             layer_name = module.name
             if layer_name == 'weight':
-                print(
-                    f'{name}{layer_name} --> '
-                    f' c_w: {round(module.c_delta.data.item(), 5)},'
-                    f' d_w: {round(module.d_delta.data.item(), 5)}')
+                logger.info(
+                    f'{name}{layer_name}\t'
+                    f'\t c_w \t {round(module.c_delta.data.item(), 5)}'
+                    f'\t d_w \t {round(module.d_delta.data.item(), 5)}')
             if layer_name == 'activation':
-                print(
-                    f'{name}{layer_name} --> '
-                    f' c_x: {round(module.c_delta.data.item(), 5)},'
-                    f' d_x: {round(module.d_delta.data.item(), 5)}')
+                logger.info(
+                    f'{name}{layer_name}\t'
+                    f'\t c_w \t {round(module.c_delta.data.item(), 5)}'
+                    f'\t d_w \t {round(module.d_delta.data.item(), 5)}')
 
